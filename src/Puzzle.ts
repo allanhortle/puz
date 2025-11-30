1f328023abc0828ccd027c17630420b96adc983a
Allan Hortle <allanhortle@gmail.com>
2025-11-30 13:36:00 +1100

    Fix the circles

--- src/Puzzle.ts
+++ src/Puzzle.ts
@@ -6,229 +6,251 @@ import handlebars from 'handlebars';
 import path from 'path';
 import {Buffer} from 'buffer';
 import qrcode from 'qrcode';
+import chalk from 'chalk';
 
 function* chunks(arr, n) {
-    for (let i = 0; i < arr.length; i += n) {
-        yield arr.slice(i, i + n);
-    }
+  for (let i = 0; i < arr.length; i += n) {
+    yield arr.slice(i, i + n);
+  }
 }
 
 type PuzzleData = {
-    down: string[];
-    across: string[];
-    fileName: string;
-    board: Array<{black: boolean; number?: number}>;
-    solution: Array<string>;
-    title: string;
-    author: string;
-    copyright: string;
-    qrCode: string;
-    notes: string;
-    width: number;
-    height: number;
-    boardLength: number;
+  down: string[];
+  across: string[];
+  fileName: string;
+  date: Date;
+  board: Array<{black: boolean; number?: number}>;
+  rawBoard: {black: boolean; number?: number, circle?: boolean}[][];
+  solution: Array<string>;
+  title: string;
+  author: string;
+  copyright: string;
+  qrCode: string;
+  notes: string;
+  width: number;
+  height: number;
+  boardLength: number;
 };
 
 export default class Puzzle {
-    data: PuzzleData;
-    constructor(data: PuzzleData) {
-        this.data = data;
+  data: PuzzleData;
+  constructor(data: PuzzleData) {
+    this.data = data;
+  }
+
+  static async fromFile(file: string) {
+    // File
+    const puz = await readFile(file);
+    const raw = new Uint8Array(puz);
+    const text = new TextDecoder('windows-1252');
+    const toString = (x) => text.decode(x);
+    const encoder = new TextEncoder();
+    const {name} = parse(file);
+    let buf = Buffer.from(raw);
+
+    // Properties
+    const width = raw[0x2c];
+    const height = raw[0x2d];
+    const boardLength = width * height;
+
+    //
+    // THE BOARDS SECTION
+    //
+    const boardStart = 0x34;
+    const stateStart = boardStart + boardLength;
+    const stringStart = stateStart + boardLength;
+    const chunkRow = new RegExp(`.{${width}}`, 'g');
+    const solution =
+      text.decode(raw.slice(boardStart, boardStart + boardLength)).match(chunkRow) || [];
+    //const state = text.decode(raw.slice(stateStart, stateStart + boardLength)).match(chunkRow);
+
+    //
+    // THE STRINGS SECTION
+    //
+    let stringData = buf.subarray(stringStart);
+
+    // Shift off a everything up to the next null
+    function shiftToNull() {
+      const end = stringData.indexOf(0x00);
+      const next = stringData.subarray(0, end);
+      stringData = stringData.subarray(end + 1);
+      return next;
+    }
+    function shiftClue() {
+      return toString(shiftToNull());
     }
 
-    static async fromFile(file: string) {
-        // File
-        const puz = await readFile(file);
-        const raw = new Uint8Array(puz);
-        const text = new TextDecoder('windows-1252');
-        const toString = (x) => text.decode(x);
-        const encoder = new TextEncoder();
-        const {name} = parse(file);
-        let buf = Buffer.from(raw);
+    const title = shiftClue();
+    const author = shiftClue();
+    const copyright = shiftClue();
 
-        // Properties
-        const width = raw[0x2c];
-        const height = raw[0x2d];
-        const boardLength = width * height;
+    //
+    // Then Do The Clues
 
-        //
-        // THE BOARDS SECTION
-        //
-        const boardStart = 0x34;
-        const stateStart = boardStart + boardLength;
-        const stringStart = stateStart + boardLength;
-        const chunkRow = new RegExp(`.{${width}}`, 'g');
-        const solution =
-            text.decode(raw.slice(boardStart, boardStart + boardLength)).match(chunkRow) || [];
-        //const state = text.decode(raw.slice(stateStart, stateStart + boardLength)).match(chunkRow);
+    // Helpers
+    function isBlack(x: number, y: number) {
+      return solution[y][x] === '.';
+    }
 
-        //
-        // THE STRINGS SECTION
-        //
-        let stringData = buf.subarray(stringStart);
-
-        // Shift off a everything up to the next null
-        function shiftToNull() {
-            const end = stringData.indexOf(0x00);
-            const next = stringData.subarray(0, end);
-            stringData = stringData.subarray(end + 1);
-            return next;
+    function needsAcrossNumber(x: number, y: number) {
+      // Check that there is no space to the left
+      if (x === 0 || isBlack(x - 1, y)) {
+        // Check that there is space (at least two cells) for a word here
+        if (x + 2 < width && !isBlack(x + 1, y)) {
+          return true;
         }
-        function shiftClue() {
-            return toString(shiftToNull());
+      }
+      return false;
+    }
+
+    function needsDownNumber(x: number, y: number) {
+      // Check that there is no space above
+      if (y === 0 || isBlack(x, y - 1)) {
+        // Check that there is space (at least two cells) for a word here
+        if (y + 2 < height && !isBlack(x, y + 1)) {
+          return true;
         }
+      }
+      return false;
+    }
 
-        const title = shiftClue();
-        const author = shiftClue();
-        const copyright = shiftClue();
+    // Work through the board one by one building up the data
+    // At each valid clue location shift off a new clue from the buffer
+    const across: string[] = [];
+    const down: string[] = [];
+    const board: {
+      black: boolean;
+      circle?: boolean;
+      number?: number;
+    }[][] = [...Array(height)].map((_) => Array(width));
 
-        //
-        // Then Do The Clues
-
-        // Helpers
-        function isBlack(x: number, y: number) {
-            return solution[y][x] === '.';
+    let currentNumber = 1;
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        if (isBlack(x, y)) {
+          board[y][x] = {black: true};
+          continue;
         }
+        const needsDown = needsDownNumber(x, y);
+        const needsAcross = needsAcrossNumber(x, y);
 
-        function needsAcrossNumber(x: number, y: number) {
-            // Check that there is no space to the left
-            if (x === 0 || isBlack(x - 1, y)) {
-                // Check that there is space (at least two cells) for a word here
-                if (x + 2 < width && !isBlack(x + 1, y)) {
-                    return true;
-                }
+        board[y][x] = {black: false};
+        if (needsAcross || needsDown) {
+          if (needsAcross) across[currentNumber] = shiftClue() ?? '';
+          if (needsDown) down[currentNumber] = shiftClue() ?? '';
+          board[y][x] = {black: false, number: currentNumber};
+          currentNumber++;
+        }
+      }
+    }
+
+    // Process Extras
+    while (stringData.length > 1) {
+      const data = shiftToNull();
+
+      if (toString(data).match(/(GEXT)|(GRBS)|(RTBL)/)) {
+        const title = toString(data.subarray(0, 4));
+        console.log({width});
+        const content = stringData.subarray(0, width * height);
+        if (title === 'RTBL') {
+          // Probably dont need to parse these for printing
+        }
+        if (title === 'GRBS') {
+          // Just rebus answers
+          // Probably dont need to parse these for printing
+        }
+        if (title === 'GEXT') {
+          content.forEach((ii, index) => {
+            const column = ((index + 1) % width);
+            const row = Math.floor(index / width);
+            if (ii > 0) {
+              console.log({spot: board[row], index, column, row});
             }
-            return false;
+            board[row][column].circle = ii > 0;
+          });
         }
-
-        function needsDownNumber(x: number, y: number) {
-            // Check that there is no space above
-            if (y === 0 || isBlack(x, y - 1)) {
-                // Check that there is space (at least two cells) for a word here
-                if (y + 2 < height && !isBlack(x, y + 1)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        // Work through the board one by one building up the data
-        // At each valid clue location shift off a new clue from the buffer
-        const across: string[] = [];
-        const down: string[] = [];
-        const board: {
-            black: boolean;
-            circle?: boolean;
-            number?: number;
-        }[][] = [...Array(height)].map((_) => Array(width));
-
-        let currentNumber = 1;
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                if (isBlack(x, y)) {
-                    board[y][x] = {black: true};
-                    continue;
-                }
-                const needsDown = needsDownNumber(x, y);
-                const needsAcross = needsAcrossNumber(x, y);
-
-                board[y][x] = {black: false};
-                if (needsAcross || needsDown) {
-                    if (needsAcross) across[currentNumber] = shiftClue() ?? '';
-                    if (needsDown) down[currentNumber] = shiftClue() ?? '';
-                    board[y][x] = {black: false, number: currentNumber};
-                    currentNumber++;
-                }
-            }
-        }
-
-        // Process Extras
-        while (stringData.length > 1) {
-            const data = shiftToNull();
-
-            if (toString(data).match(/(GEXT)|(GRBS)|(RTBL)/)) {
-                const title = toString(data.subarray(0, 4));
-                const length = data[4];
-                const content = stringData.subarray(2, length + 2);
-                if (title === 'RTBL') {
-                    // Probably dont need to parse these for printing
-                }
-                if (title === 'GRBS') {
-                    // Probably dont need to parse these for printing
-                }
-                if (title === 'GEXT') {
-                    content.forEach((ii, index) => {
-                        const column = index % width;
-                        const row = Math.floor(index / width);
-                        board[row][column].circle = ii > 0;
-                    });
-                }
-            }
-        }
-
-        // QRCode
-        const d = new Date(title);
-        const answerUrl = `https://www.xwordinfo.com/Crossword?date=${
-            d.getMonth() + 1
-        }/${d.getDate()}/${d.getFullYear()}`;
-        const qrCode = await qrcode.toDataURL(answerUrl);
-
-        return new Puzzle({
-            down,
-            across,
-            fileName: name,
-            board: board.flat(1),
-            solution,
-            title,
-            author,
-            copyright,
-            notes: '',
-            width,
-            height,
-            boardLength,
-            qrCode
-        });
+      }
     }
 
-    get stats() {
-        return {
-            title: this.data.title,
-            author: this.data.author,
-            copyright: this.data.copyright,
-            notes: this.data.notes,
-            width: this.data.width,
-            height: this.data.height,
-            acrossClues: this.data.across.filter((ii) => ii).length,
-            downClues: this.data.down.filter((ii) => ii).length,
-            fileName: this.data.fileName
-        };
-    }
+    // QRCode
+    console.log(title);
+    const d = new Date(title);
+    const answerUrl = `https://www.xwordinfo.com/Crossword?date=${d.getMonth() + 1
+      }/${d.getDate()}/${d.getFullYear()}`;
+    const qrCode = await qrcode.toDataURL(answerUrl);
 
-    async pdf(options: {output?: string; debugTemplate?: boolean} = {}) {
-        const {output = process.cwd(), debugTemplate} = options;
-        const stats = await lstat(output);
-        const outputPath = stats.isDirectory()
-            ? path.join(output, `${this.data.fileName}.pdf`)
-            : output;
+    return new Puzzle({
+      down,
+      across,
+      fileName: name,
+      date: d,
+      board: board.flat(1),
+      rawBoard: board,
+      solution,
+      title,
+      author,
+      copyright,
+      notes: '',
+      width,
+      height,
+      boardLength,
+      qrCode
+    });
+  }
 
-        const template = handlebars.compile(
-            await readFile(path.join(__dirname, '../../src/template.hbs'), 'utf8')
-        );
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        const html = template(this.data);
-        if (debugTemplate) await writeFile('template.html', html, 'utf8');
+  get stats() {
+    return {
+      title: this.data.title,
+      author: this.data.author,
+      copyright: this.data.copyright,
+      notes: this.data.notes,
+      width: this.data.width,
+      height: this.data.height,
+      date: this.data.date,
+      acrossClues: this.data.across.filter((ii) => ii).length,
+      downClues: this.data.down.filter((ii) => ii).length,
+      fileName: this.data.fileName
+    };
+  }
 
-        await page.goto(`data:text/html;charset=UTF-8,${encodeURIComponent(html)}`);
-        await page.pdf({
-            path: outputPath,
-            format: 'A4',
-            printBackground: true
-        });
-        await browser.close();
-        return outputPath;
-    }
+  async pdf(options: {output?: string; debugTemplate?: boolean} = {}) {
+    const {output = process.cwd(), debugTemplate} = options;
+    const stats = await lstat(output);
+    const outputPath = stats.isDirectory()
+      ? path.join(output, `${this.data.fileName}.pdf`)
+      : output;
 
-    printSolution() {
-        console.log(this.data.solution.join('\n').replace(/\./g, ' ').replace(/(.)/g, '$1 '));
-    }
+    const template = handlebars.compile(
+      await readFile(path.join(__dirname, '../../src/template.hbs'), 'utf8')
+    );
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const html = template(this.data);
+    if (debugTemplate) await writeFile('template.html', html, 'utf8');
+
+    await page.goto(`data:text/html;charset=UTF-8,${encodeURIComponent(html)}`);
+    await page.pdf({
+      path: outputPath,
+      format: 'A4',
+      printBackground: true
+    });
+    await browser.close();
+    return outputPath;
+  }
+
+  printSolution() {
+    const {rawBoard} = this.data;
+
+    const output = this.data.solution.map((row, rowIndex) => {
+      return row.split('').map((col, colIndex) => {
+        const data = rawBoard[rowIndex][colIndex];
+        if ('circle' in data) {
+          if (data.circle) return chalk.green(col);
+          return chalk.gray(col);
+        }
+        return col;
+      }).join(' ');
+    });
+    console.log(output.join('\n').replace(/\./g, ' '));
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,66 @@
 #!/usr/bin/env node
 import {Command} from 'commander';
-import pkg from '../package.json';
-import Puzzle from './Puzzle';
+import pkg from '../package.json' with {type: "json"};
+import Puzzle from './Puzzle.js';
 import {getPrinters, print} from 'unix-print';
 import prompts from 'prompts';
 
 const program = new Command('puz').description('cli to work with .puz files').version(pkg.version);
 
 program
-    .command('pdf <file>')
-    .description('convert .puz to .pdf')
-    .option('-o, --output <string>', 'output location')
-    .option('-d, --debugTemplate', 'export pdf template as html')
-    .action(async (file, {output, debugTemplate}) => {
-        const puzzle = await Puzzle.fromFile(file);
-        console.log(await puzzle.pdf({output, debugTemplate}));
-    });
+  .command('pdf <file>')
+  .description('convert .puz to .pdf')
+  .option('-o, --output <string>', 'output location')
+  .option('-d, --debugTemplate', 'export pdf template as html')
+  .action(async (file, {output, debugTemplate}) => {
+    const puzzle = await Puzzle.fromFile(file);
+    console.log(await puzzle.pdf({output, debugTemplate}));
+  });
 
 program
-    .command('print <file>')
-    .description('skip the pdf and go straight to the printer')
-    .option('-o, --output <string>', 'output location')
-    .action(async (file: string, {output}) => {
-        try {
-            const puzzle = await Puzzle.fromFile(file);
-            const filepath = await puzzle.pdf({output});
-            const printers = await getPrinters();
-            const {printer} = await prompts([
-                {
-                    type: 'select',
-                    name: 'printer',
-                    message: 'Which printer?',
-                    choices: printers.map((ii) => ({
-                        title: ii.description ?? '',
-                        value: ii.printer
-                    }))
-                }
-            ]);
-            console.log('printing', filepath);
-            await print(filepath, printer);
-        } catch (e) {
-            console.error(e);
+  .command('print <file>')
+  .description('skip the pdf and go straight to the printer')
+  .option('-o, --output <string>', 'output location')
+  .action(async (file: string, {output}) => {
+    try {
+      const puzzle = await Puzzle.fromFile(file);
+      const filepath = await puzzle.pdf({output});
+      const printers = await getPrinters();
+      const {printer} = await prompts([
+        {
+          type: 'select',
+          name: 'printer',
+          message: 'Which printer?',
+          choices: printers.map((ii) => ({
+            title: ii.description ?? '',
+            value: ii.printer
+          }))
         }
-    });
+      ]);
+      console.log('printing', filepath);
+      await print(filepath, printer);
+    } catch (e) {
+      console.error(e);
+    }
+  });
 
 program
-    .command('stats <file>')
-    .description('print the puzzle details')
-    .option('--json', 'output as json')
-    .action(async (file, options) => {
-        const puzzle = await Puzzle.fromFile(file);
-        if (options.json) return console.log(JSON.stringify(puzzle.stats));
-        console.log(puzzle.stats);
-    });
+  .command('stats <file>')
+  .description('print the puzzle details')
+  .option('--json', 'output as json')
+  .action(async (file, options) => {
+    const puzzle = await Puzzle.fromFile(file);
+    if (options.json) return console.log(JSON.stringify(puzzle.stats));
+    console.log(puzzle.stats);
+  });
 
 program
-    .command('solution')
-    .description('show the puzzle solution')
-    .argument('<file>', '.puz file')
-    .action(async (file) => {
-        const puzzle = await Puzzle.fromFile(file);
-        puzzle.printSolution();
-    });
+  .command('solution')
+  .description('show the puzzle solution')
+  .argument('<file>', '.puz file')
+  .action(async (file) => {
+    const puzzle = await Puzzle.fromFile(file);
+    puzzle.printSolution();
+  });
 
 program.parse();
